9d15ebb9054a59c9e75d290d1c00e2c0d1ec895e
Chrisotpher Steel <chris.steel@gmail.com>
2019-04-08 13:33:54 -0400

    edited scripts/github-delete-draft-release.sh

--- scripts/github-delete-draft-release.sh
+++ scripts/github-delete-draft-release.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #
-# scripts/github-delete-draft-release.sh
+# github-delete-draft-release.sh
 #
 source ~/.private/github/$GIT_ACCOUNT
 source scripts/github-release-variables.sh
 source scripts/github-release-variables-test.sh
+github-release delete \
   --owner $GIT_ACCOUNT \
   --repo $GIT_REPOSITORY \
   --tag $GIT_TAG \
   --name $GIT_TAG \
   --body "Draft release $GIT_ACCOUNT.$GIT_SHORT_NAME $GIT_TAG" \
-  --draft
+  --draft=false
