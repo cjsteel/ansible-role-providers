692191da999cfe64e7ca6d6303ebd811931bc172
Chrisotpher Steel <chris.steel@gmail.com>
2019-04-09 14:39:23 -0400

    commit prerelease v0.0.7 changes

--- scripts/github-draft-release.sh
+++ scripts/github-draft-release.sh
@@ -5,6 +5,7 @@
 source ~/.private/github/$GIT_ACCOUNT
 source scripts/github-release-variables.sh
 source scripts/github-release-variables-test.sh
+github-release upload \
   --owner $GIT_ACCOUNT \
   --repo $GIT_REPOSITORY \
   --tag $GIT_TAG \
